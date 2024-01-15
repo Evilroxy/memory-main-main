750e51f68d5903037bc934d5d5525e24c60cf011
Evilroxy <cvanelstraete@edenschool.fr>
2024-01-15 17:16:49 +0100

    change clicked1

--- main.js
+++ main.js
@@ -1,34 +1,41 @@
 let cards = Array.from(document.querySelectorAll(".card"));
-let clicked1;
-let clicked2;
+let clicked1 = "";
+let flag = false;
 const btnnew = document.getElementById("#btnnew");
 
 for (let i = 0; i < cards.length; i++) {
   cards[i].addEventListener("click", () => {
-    cards[i].classList.add("open");
-    if (!clicked1) {
-      clicked1 = cards[i];
-    } else {
-      clicked2 = cards[i];
-      verify();
+    if (flag == false) {
+      cards[i].classList.add("open");
+      if (clicked1 == "") {
+        clicked1 = cards[i];
+      } else if (
+        clicked1.getAttribute("data-key") !== cards[i].getAttribute("data-key")
+      ) {
+        flag = true;
+        setTimeout(() => {
+          clicked1.classList.remove("open");
+          cards[i].classList.remove("open");
+          clicked1 = "";
+          flag = false;
+        }, 1000);
+      } else {
+        clicked1 = "";
+      }
     }
-    console.log(clicked1, clicked2);
   });
 }
 
-function verify() {
-  if (clicked1.getAttribute("data-key") === clicked2.getAttribute("data-key")) {
-    clicked1.classList.add("open");
-    clicked2.classList.add("open");
-  } else {
-    setTimeout(() => {
-      clicked1.classList.remove("open");
-      clicked2.classList.remove("open");
-    }, 1000);
-  }
-  clicked1;
-  clicked2;
-}
+// function verify() {
+//   if (clicked1.getAttribute("data-key") !== cards[i].getAttribute("data-key")) {
+//       setTimeout(() => {
+//         clicked1.classList.remove("open");
+//         cards[i].classList.remove("open");
+//       }, 1000);
+//   }
+//   clicked1;
+//   cards[i];
+// }
 
 /*function melange() {
   const results = [];
